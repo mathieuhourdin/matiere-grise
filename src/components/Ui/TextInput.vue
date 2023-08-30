6aaa0507078c8eb25921314cf03b9decc326ebb2
mathieu <mathieu@fleursdici.fr>
2023-08-31 00:54:51 +0200

    style in form and add button to create article

--- src/components/Ui/TextInput.vue
+++ src/components/Ui/TextInput.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="m-4">
     <label class="block text-2xs text-slate-800">{{ label }}</label>
-    <input class="border border-neutral-400 block h-full w-full rounded-md border-2" :value="modelValue" :placeholder="placeholder" @input="event => onInput(event)" />
+    <input
+      class="border border-neutral-400 block h-full w-full rounded-md border-2 p-1 pl-2 text-xs"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="(event) => onInput(event)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-  const emit = defineEmits(['update:modelValue']);
-  const props = defineProps<{
-    label?: String,
-    modelValue?: String,
-    placeholder?: String,
-  }>();
-  const onInput = (event) => {
-    emit("update:modelValue", event.target.value);
-  };
+const emit = defineEmits(['update:modelValue', 'input'])
+const props = defineProps<{
+  label?: String
+  modelValue?: String
+  placeholder?: String
+}>()
+const onInput = (event) => {
+  emit('update:modelValue', event.target.value)
+  emit('input', event.target.value)
+}
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,19 @@
-<script setup lang="ts">
-import HomeBanner from '../components/HomeBanner.vue'
-import ArticlesList from '../components/ArticlesList.vue'
-</script>
-
 <template>
   <main>
     <HomeBanner />
     <div class="max-w-3xl mx-auto px-8">
-      <p class="m-6 text-center">Bienvenue, sur le site de Mathieu Hourdin qui va parler de plein de choses différentes, notamment de comment sauver le monde, et s'amuser dans la vie, rien que ça.
+      <p class="m-6 text-center">
+        Bienvenue, sur le site de Mathieu Hourdin qui va parler de plein de choses différentes,
+        notamment de comment sauver le monde, et s'amuser dans la vie, rien que ça.
       </p>
+      <AddArticleButton />
       <ArticlesList />
     </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import HomeBanner from '../components/HomeBanner.vue'
+import ArticlesList from '../components/ArticlesList.vue'
+import AddArticleButton from '@/components/AddArticleButton.vue'
+</script>
