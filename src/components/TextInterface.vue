87ff34ea236e0ba8204606f7b571628c494f07cd
mathieu <mathieu@fleursdici.fr>
2023-08-31 15:43:50 +0200

    can write in TextInterface

--- src/components/TextInterface.vue
+++ src/components/TextInterface.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="border-2 h-80 mx-auto relative" style="width: 800px">
+  <div
+    class="border-2 h-80 mx-auto relative"
+    tabindex="0"
+    style="width: 800px"
+    @keydown="handleWrite"
+  >
     <div
       class="absolute text-center"
       style="width: 80px, height: 80px"
@@ -13,7 +18,8 @@
 </template>
 
 <script setup lang="ts">
-const textArray = [
+import { ref } from 'vue'
+const textArray = ref([
   'J',
   'e',
   ' ',
@@ -34,18 +40,27 @@ const textArray = [
   'i',
   'e',
   'u'
-]
-const writerWidth = 800;
-const letterWidth = 15;
-const letterHeight = 20;
-const lettersCountByLine = writerWidth / letterWidth;
+])
+const handleWrite = (event) => {
+  console.log('Writes : ', event.key)
+  const key = event.key;
+  if (key.length == 1) {
+    textArray.value.push(event.key)
+  } else if (key == "Backspace") {
+    textArray.value.pop();
+  }
+}
+const writerWidth = 800
+const letterWidth = 10
+const letterHeight = 15
+const lettersCountByLine = writerWidth / letterWidth
 const calculateLeftFromIndex = (index) => {
-  const width = `${letterWidth}px`;
-  const height = `${letterHeight}px`;
-  const top = `${letterHeight * Math.floor(index / lettersCountByLine)}px`;
-  const left = `${(letterWidth * (index % lettersCountByLine)) + 1}px`;
-  const fontSize = "80%";
-  const padding = "0.5px";
-  return { width, height, left, top, fontSize, padding };
-};
+  const width = `${letterWidth}px`
+  const height = `${letterHeight}px`
+  const top = `${letterHeight * Math.floor(index / lettersCountByLine)}px`
+  const left = `${letterWidth * (index % lettersCountByLine) + 1}px`
+  const fontSize = '70%'
+  const padding = '0.5px'
+  return { width, height, left, top, fontSize, padding }
+}
 </script>
