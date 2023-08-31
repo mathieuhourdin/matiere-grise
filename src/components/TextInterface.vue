d10ae64e93d67e32150af8ffa6dc08eea17fbcb6
mathieu <mathieu@fleursdici.fr>
2023-08-31 19:14:15 +0200

    working text interface

--- src/components/TextInterface.vue
+++ src/components/TextInterface.vue
@@ -9,94 +9,79 @@
       v-for="(line, lindex) in lines"
       :key="lindex"
       :style="{ height: line.height }"
-      class="flex"
+      class="flex flex-wrap w-full"
     >
-      <div :id="`field_${lindex}`" class="flex">
         <div
-          v-for="(letter, tindex) in getLettersForLine(lindex)"
+          v-for="(letter, tindex) in line.text"
           :key="tindex"
-          :class="{ 'border-r-4': letter.id == currentCursorPosition }"
+          class="border-blue-400"
+          :class="{ 'border-r-2': letter.id == currentCursorPosition?.id }"
           @click="(event) => selectCursorPosition(event, letter)"
         >
           <div v-if="letter.char == ' '" style="width: 5px" />
+          <div v-else-if="letter.char =='\n'" style="height: 25px" />
           <div v-else>
             <div>{{ letter.char }}</div>
           </div>
-        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const textArray = ref([
-  'J',
-  'e',
-  ' ',
-  'm',
-  "'",
-  'a',
-  'p',
-  'p',
-  'e',
-  'l',
-  'l',
-  'e',
-  ' ',
-  'M',
-  'a',
-  't',
-  'h',
-  'i',
-  'e',
-  'u'
-])
-const lines = [{ height: 20 }]
+import { ref, computed } from 'vue'
+
+const lines = computed(() => {
+  console.log("Compute lines");
+  let ids = [{id: 0}].concat(text.value.filter((char) => char.char == "\n"));
+  console.log("Compute ids.length : ", ids.length);
+  for (let i = 0; i < ids.length -1; i++) {
+    console.log("Compute line : ", i)
+    ids[i].text = text.value.filter((char) => char.id >= ids[i].id && char.id < ids[i+1].id);
+  }
+  ids[ids.length -1].text = text.value.filter((char) => char.id >= ids[ids.length -1].id);
+  return ids;
+})
+
 const text = ref([
   { id: 0, char: 'M', line: 0 },
   { id: 1, char: 'a', line: 0 }
 ])
-const getLineWidth = (index) => {
-  console.log('Index : ', index)
-  return document.getElementById(`field_${index}`).offsetWidth
-}
+
 const selectCursorPosition = (event, letter) => {
   console.log('selectCursorPosition letter : ', letter)
-  currentCursorPosition.value = letter.id
+  currentCursorPosition.value = { id: letter.id, line: letter.line, char: letter.char }
 }
 const currentCursorPosition = ref(null)
+
+const insertChar = (key) => {
+  text.value
+    .filter((letter) => {
+      return letter.id >= currentCursorPosition.value.id + 1
+    })
+    .forEach((letter) => (letter.id += 1))
+  text.value.splice(currentCursorPosition.value.id + 1, 0, {
+    id: currentCursorPosition.value.id + 1,
+    char: key,
+    line: 0
+  })
+  currentCursorPosition.value.id += 1
+}
 
 const handleWrite = (event) => {
   console.log('Writes : ', event.key)
   if (currentCursorPosition.value === null) return
   const key = event.key
   if (key.length == 1) {
-    textArray.value.push(event.key)
-    text.value
-      .filter((letter) => {
-        return letter.id >= currentCursorPosition.value + 1
-      })
-      .forEach((letter) => (letter.id += 1))
-    text.value.splice(currentCursorPosition.value + 1, 0, {
-      id: currentCursorPosition.value + 1,
-      char: event.key,
-      line: 0
-    })
-    currentCursorPosition.value += 1
+    insertChar(key);
   } else if (key == 'Backspace') {
-    text.value.splice(currentCursorPosition.value, 1)
+    text.value.splice(currentCursorPosition.value.id, 1)
     text.value
-      .filter((letter) => letter.id > currentCursorPosition.value)
+      .filter((letter) => letter.id > currentCursorPosition.value.id)
       .forEach((letter) => (letter.id -= 1))
-    currentCursorPosition.value -= 1
-    textArray.value.pop()
+    currentCursorPosition.value.id -= 1
+  } else if (key == 'Enter') {
+    insertChar("\n");
   }
-}
-const getLettersForLine = (index) => {
-  console.log('getLettersForLine index : ', index)
-  let letters = text.value.filter((letter) => letter.line == index)
-  console.log('getted letters : ', letters)
-  return letters
 }
 </script>
