55f95221fd276e495d8bfe16459ba8131607cf67
mathieu <mathieu@fleursdici.fr>
2023-09-01 13:32:02 +0200

    cover image in article list

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -3,8 +3,10 @@
     <router-link :to="articleLink">
       <img
         :src="imageUrl"
-        class="border border-slate-300 dark:border-zinc-700 rounded-xl max-w-full aspect-[2/1]"
+        class="border border-slate-300 dark:border-zinc-700 rounded-xl max-w-full aspect-[2/1] object-cover object-center"
         width="500"
+        fit="cover"
+        position="center"
       />
       <ProgressBar :progress-value="progress" class="w-1/3" />
       <div class="mt-3 text-xl font-bold">{{ title }}</div>
