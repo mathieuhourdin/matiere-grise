358cffb7e9e881ecfa5f8946307c11b5bfae94fd
mathieu <mathieu@fleursdici.fr>
2023-09-01 11:58:33 +0200

    deploy dist

--- dist/assets/WriterView-17ebd161.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./TextInterface.vue_vue_type_script_setup_true_lang-85718c93.js";import{e as o,o as n,c as s,a as c}from"./index-5cb82f41.js";const i=o({__name:"WriterView",setup(l){const t=e=>{console.log("Changed the text: ",e)};return(e,r)=>(n(),s("div",null,[c(a,{"full-text":"Salut à tous c'est le texte",class:"max-w-xl m-10 h-96",onChange:t})]))}});export{i as default};
