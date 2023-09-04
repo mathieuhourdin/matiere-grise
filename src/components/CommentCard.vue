8a9ac821a8dc692175cb42dd31bbb3bdb205887a
mathieu <mathieu@fleursdici.fr>
2023-09-04 11:05:45 +0200

    better comment card, with button and user name

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -1,16 +1,24 @@
 <template>
-  <div class="bg-slate-100 border-2 p-4 m-1 rounded">
-    <TextAreaInput class="w-full" :modelValue="modelValue" @update:modelValue="onInput" />
+  <div class="bg-slate-100 border-2 p-4 m-1 rounded p-2">
+    <div class="text-xs m-1">{{ user.first_name }} {{ user.last_name }}</div>
+    <textarea class="w-full text-xs rounded-xl p-2" :value="modelValue" />
+    <div class="flex">
+      <ActionButton class="text-sm" rounded size="2xs" text="Commenter" type="valid" />
+      <ActionButton class="text-sm" rounded size="2xs" text="Annuler" type="abort" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import TextAreaInput from '@/components/Ui/TextAreaInput.vue'
+import ActionButton from '@/components/Ui/ActionButton.vue'
+import { useUser } from '@/composables/useUser.ts'
 const emit = defineEmits(['update:modelValue'])
 const props = defineProps<{
   editing: boolean
   modelValue: string
 }>()
+const { user } = useUser();
 const onInput = (event) => {
   emit('update:modelValue', event)
 }
--- src/components/Ui/ActionButton.vue
+++ src/components/Ui/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <button
-      :class="buttonClass"
+      :class="`${buttonClass} ${textSize(size)} ${roundedClass(rounded)}`"
       @click="clickOnButton()"
         >{{ text }}</button>
   </div>
@@ -10,13 +10,37 @@
 <script setup lang="ts">
   import { computed } from 'vue';
   const emit = defineEmits(['click']);
-  const props = defineProps<{
+  const props = withDefaults(defineProps<{
     text: String,
     type: String,
-  }>();
+    size?: String,
+    rounded?: boolean,
+  }>(), {
+    size: "base",
+    rounded: false
+  });
+  const textSize = (size) => {
+    if (size == "2xs") {
+      return "py-0.5 px-1 text-2xs";
+    }
+    if (size == "xs") {
+      return "py-1 px-2 text-xs";
+    }
+    if (size == "sm") {
+      return "py-1.5 px-3 text-sm";
+    }
+    if (size == "base") {
+      return "py-2 px-4 text-base";
+    }
+    return "py-2 px-4 text-" + size;
+  };
+  const roundedClass = (rounded) => {
+    if (rounded) return "rounded-xl";
+    return "rounded";
+  };
   const buttonClass = computed(() => {
-    if (props.type == "valid") return "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" ;
-    if (props.type == "abort") return "bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow";
+    if (props.type == "valid") return "bg-blue-500 hover:bg-blue-700 text-white font-bold" ;
+    if (props.type == "abort") return "bg-white hover:bg-gray-100 text-gray-800 font-semibold border border-gray-400 shadow";
     return "";
   });
   const clickOnButton = () => {
