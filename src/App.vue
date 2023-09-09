82b2d5c91216a7f99725be509cf6167cd6dc38f5
mathieu <mathieu@fleursdici.fr>
2023-09-09 15:00:30 +0200

    menu hidden

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import { RouterLink, RouterView } from 'vue-router'
 import AppHeader from './components/AppHeader.vue'
 import SidebarMenu from '@/components/SidebarMenu.vue'
+import { ref } from 'vue'
+const menuOpen = ref(false)
 </script>
 
 <template>
   <AppHeader />
   <div class="flex">
-    <SidebarMenu style="min-height: 800px" class="md:w-250px w-80pt absolute md:static" />
-    <RouterView style="width: 1450px"/>
+    <SidebarMenu v-if="menuOpen" style="min-height: 800px" class="md:w-250px w-80pt absolute md:static" />
+    <RouterView :style="{width: menuOpen ? '1450px' : '100%'}"/>
   </div>
 </template>
