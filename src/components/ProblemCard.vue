1bdc35185b67da195e6e8c4bbb396f0388ab5e90
mathieu <mathieu@fleursdici.fr>
2023-09-14 13:01:04 +0200

    input count on problem card and better style

--- src/components/ProblemCard.vue
+++ src/components/ProblemCard.vue
@@ -4,14 +4,14 @@
       <div class="max-h-full">
         <img class="h-28 mx-auto mb-4" :src="problem.image_url" />
         <div class="h-full">
-          <div>{{ problem.title }}</div>
+          <div class="mb-2">{{ problem.title }}</div>
           <div class="text-2xs italic" style="margin-top: -8px">
             {{ problem.owner_id }}
           </div>
           <div class="text-2xs mb-auto">{{ formatText(problem.description) }}</div>
-          <div class="flex mt-auto">
+          <div class="flex mt-2">
             <div class="text-2xs italic mt-auto">{{ formatDate(problem.created_at) }}</div>
-            <div class="ml-auto text-2xs underline">{{ problem.inputs }} inputs</div>
+            <div class="ml-auto text-2xs underline">{{ thoughtInputs.length }} inputs</div>
           </div>
         </div>
       </div>
@@ -21,12 +21,16 @@
 
 <script setup lang="ts">
 import { Problem } from '@/composables/useProblem.ts'
+import { useThoughtInputUsages } from '@/composables/useThoughtInputUsages.ts'
+import { onMounted, ref } from 'vue'
+
 const props = defineProps<{
   problem: Problem
 }>()
 
-const formatDate = (date: Date) => {
-  return date.toLocaleString('fr-FR', {
+const formatDate = (date: string) => {
+  let formatDate = new Date(date.split(".")[0])
+  return formatDate.toLocaleString('fr-FR', {
     hour: 'numeric',
     weekday: 'short',
     day: 'numeric',
@@ -34,7 +38,17 @@ const formatDate = (date: Date) => {
     year: '2-digit'
   })
 }
+
 const formatText = (text) => {
   return text.length > 200 ? text.slice(0, 150) + '...' : text
 }
+
+const thoughtInputs = ref([])
+const { getThoughtInputUsagesForThoughtOutput } = useThoughtInputUsages()
+const loadThoughtInputs = async () => {
+  thoughtInputs.value = await getThoughtInputUsagesForThoughtOutput(props.problem.id)
+}
+
+onMounted(() => loadThoughtInputs())
+
 </script>
