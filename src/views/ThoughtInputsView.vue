886af8c4459a085b41732f21b08b6e3a10de6a77
mathieu <mathieu@fleursdici.fr>
2023-09-15 19:07:52 +0200

    can add inputs from inputs view

--- src/views/ThoughtInputsView.vue
+++ src/views/ThoughtInputsView.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="m-4">
     <div>Derniers apports</div>
+    <div class="italic underline text-xs" @click="openNewThoughtInput = true">
+      Ajouter un nouvel apport
+    </div>
+    <ModalSheet :open="openNewThoughtInput" @close="openNewThoughtInput = false">
+      <CreateThoughtInput @close="openNewThoughtInput = false" />
+    </ModalSheet>
     <ThoughtInputsList :thought-inputs="thoughtInputs" />
   </div>
 </template>
 
 <script setup lang="ts">
+import ModalSheet from '@/components/Ui/ModalSheet.vue'
+import CreateThoughtInput from '@/components/CreateThoughtInput.vue'
 import ThoughtInputsList from '@/components/ThoughtInputsList.vue'
 import { useThoughtInputs } from '@/composables/useThoughtInputs.ts'
 import { ref, onMounted } from 'vue'
+
+const openNewThoughtInput = ref(false)
 
 const { getThoughtInputs } = useThoughtInputs()
 
