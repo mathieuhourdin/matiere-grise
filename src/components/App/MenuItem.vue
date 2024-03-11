5c278644e2445287958176a57bf2696e21508d76
mathieu <mathieu@fleursdici.fr>
2024-03-11 12:39:08 +0100

    fix style in menu

--- src/components/App/MenuItem.vue
+++ src/components/App/MenuItem.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="h-12 border-b border-slate-400 flex">
-    <div class="mx-4 w-max">
-      <router-link @click="clickLink" :to="to">
-        <div class="mt-1">{{ title }}</div>
-        <div v-if="subtitle" class="text-xs text-gray-600 inline-block mb-1">{{ subtitle }}</div>
-      </router-link>
-    </div>
+    <router-link class="inline-block mx-4 w-max" @click="clickLink" :to="to">
+      <div class="text-sm mt-2">{{ title }}</div>
+      <div v-if="subtitle" class="text-2xs text-gray-600 mb-auto">{{ subtitle }}</div>
+    </router-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useMenu } from '@/composables/useMenu'
 const props = defineProps<{
-  to: string,
-  title: string,
+  to: string
+  title: string
   subtitle?: string
 }>()
 const { isMobile, setMenuOpen } = useMenu()
