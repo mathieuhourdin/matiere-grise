13f9e0023deb8520f6923fadaae303c261ae1573
mathieu <mathieu@fleursdici.fr>
2024-04-04 22:16:05 +0200

    fix spy avatar

--- src/components/User/UserAvatar.vue
+++ src/components/User/UserAvatar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-8">
     <img
-      :src="!isSpy && user && user.profile_picture_url ? user.profile_picture_url : '/assets/spy-icon.svg'"
+      :src="!isSpy && user && user.profile_picture_url ? user.profile_picture_url : spyAvatar"
       class="border-2 border-slate-300 dark:border-zinc-700 rounded-full max-w-full aspect-[2/2] object-cover object-center w-full"
     />
   </div>
@@ -9,7 +9,7 @@
 
 <script setup lang="ts">
 import { type User } from '@/types/models'
-import SpyAvatar from '@/assets/spy-icon.svg'
+import spyAvatar from '@/assets/spy-icon.svg'
 
 const props = defineProps<{
   user?: User
