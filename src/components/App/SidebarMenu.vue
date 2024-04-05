5cca4978477c412b217d784475904b8cd18c04c0
mathieu <mathieu@fleursdici.fr>
2024-04-05 11:45:36 +0200

    add menu item for feed

--- src/components/App/SidebarMenu.vue
+++ src/components/App/SidebarMenu.vue
@@ -2,6 +2,7 @@
   <div class="bg-slate-200 z-10">
     <div>
       <MenuItem v-if="user" :to="'/users/' + user.id" title="Mon profil" subtitle="Informations personnelles"/>
+      <MenuItem to="/feed" title="Fil d'actu" subtitle="Les travaux de vos amis"/>
       <MenuItem to="/problems" title="Problématiques" subtitle="Carrefours des réflexions"/>
       <MenuItem to="/productions" title="Productions" subtitle="Internes à la plateforme" />
       <MenuItem to="/resources" title="Ressources" subtitle="Externes à la plateforme" />
