8efb88c573b8d9e5934afe260b8cb3d659d2290b
mathieu <mathieu@fleursdici.fr>
2024-04-05 17:32:04 +0200

    deploy dist

--- assets/useResourceRelations-ccc2d675.js
+++
@@ -1,1 +0,0 @@
-import{E as s}from"./index-48df05bc.js";const t=async e=>(await s.get("/resource/"+e+"/bibliography")).data,o=async e=>(await s.get("/resource/"+e+"/usages")).data,a=async e=>(await s.post("/thought_input_usages",e)).data;function c(){return{getResourceRelationsForResource:t,createResourceRelation:a,getUsagesForResource:o}}export{c as u};
