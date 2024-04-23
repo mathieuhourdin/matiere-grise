bce78cdbe45481af4b11366dbf116def55c8b4a1
mathieu <mathieu@fleursdici.fr>
2024-04-23 11:40:49 +0200

    clean selectionInterface and split text managementin one composable

--- src/components/SelectionTextInterface.vue
+++ src/components/SelectionTextInterface.vue
@@ -4,11 +4,12 @@
       <div class="cursor bg-blue-400" />
     </div>
     <ClipboardButton class="ml-auto h-6 w-6" :text="fullText" />
-    <div id="editor-interface" tabindex="0" @keydown="(event) => keydown(event)">
+    <div id="editor-interface" tabindex="0" @keydown="(event) => handleKeydown(event)">
       <div
         class="whitespace-pre-wrap"
         :class="{ 'font-bold': line.bold }"
         @click="setCursorPositionFromClick(index)"
+        @mouseup="mouseupHandler"
         v-for="(line, index) in textLines"
         :id="`line-${componentUuid}-${index}`"
       >
@@ -28,6 +29,7 @@ import ClipboardButton from '@/components/ClipboardButton.vue'
 import { onMounted, ref, computed, watch } from 'vue'
 import { v4 as uuidv4 } from 'uuid'
 import { useSnackbar } from '@/composables/useSnackbar'
+import { useWrite } from '@/composables/useWrite'
 
 const emit = defineEmits(['change'])
 
@@ -38,19 +40,27 @@ const props = defineProps<{
 
 const { launchSnackbar } = useSnackbar()
 
+const { textLines, keydown, editCount } = useWrite()
+
 const componentUuid = ref<string>('')
 
 const fullText = computed(() => {
   return formatText()
 })
 
-const lastKeyPress = ref<string | null>(null)
+const mouseupHandler = (event) => {
+  console.log(event)
+  const selection = window.getSelection()
+  console.log('selection : ', selection)
+  const range = selection.getRangeAt(0)
+  console.log('range : ', range)
+}
 
 const setCursorPositionFromClick = (index) => {
   if (!props.editable) return
   const selection = window.getSelection()
   const range = selection.getRangeAt(0)
-  cursorPosition.value = { line: index, charRange: range.startOffset }
+  cursorPosition.value = { line: index, startOffset: range.startOffset, endOffset: range.endOffset }
 }
 
 const getCursorCoordinates = () => {
@@ -69,7 +79,7 @@ const getCursorCoordinates = () => {
 
   const textNode = selectedLine.firstChild.firstChild
   range.setStart(textNode, 0)
-  range.setEnd(textNode, cursorPosition.value.charRange)
+  range.setEnd(textNode, cursorPosition.value.startOffset)
 
   const boundingRects = range.getClientRects()
   const lastRect = boundingRects[boundingRects.length - 1]
@@ -82,134 +92,14 @@ const getCursorCoordinates = () => {
   }
 }
 
-const insertAt = (original: string, insertion: string, index: number) => {
-  return original.slice(0, index) + insertion + original.slice(index)
-}
-
-const removeAt = (original: string, index: number) => {
-  return original.slice(0, index - 1) + original.slice(index)
-}
-
-const editCount = ref<number>(0)
-
-const handleBackspace = async () => {
-  if (cursorPosition.value.charRange !== 0) {
-    textLines.value[cursorPosition.value.line].text = removeAt(
-      textLines.value[cursorPosition.value.line].text,
-      cursorPosition.value.charRange
-    )
-    cursorPosition.value.charRange -= 1
-  } else {
-    if (textLines.value[cursorPosition.value.line].chip) {
-      textLines.value[cursorPosition.value.line].chip = false
-      editCount.value += 1
-      return
-    }
-    if (textLines.value[cursorPosition.value.line].chip) {
-      textLines.value[cursorPosition.value.line].chip = false
-      editCount.value += 1
-      return
-    }
-    if (cursorPosition.value.line === 0) return
-
-    await new Promise((resolve) => setTimeout(resolve, 10))
-    cursorPosition.value = {
-      line: cursorPosition.value.line - 1,
-      charRange: textLines.value[cursorPosition.value.line - 1].text.length
-    }
-    textLines.value[cursorPosition.value.line].text +=
-      textLines.value[cursorPosition.value.line + 1].text
-    textLines.value.splice(cursorPosition.value.line + 1, 1)
-  }
-  editCount.value += 1
-}
-
-const handleWrite = async (key: string) => {
-  if (lastKeyPress.value === 'Dead') {
-    switch (key) {
-      case 'e':
-        key = 'ê'
-        break;
-      case 'o':
-        key = 'ô'
-        break;
-      case 'a':
-        key = 'â'
-        break;
-      case 'u':
-        key = 'û'
-        break;
-    }
-  }
-  if (key === '#' && cursorPosition.value.charRange === 0) {
-    textLines.value[cursorPosition.value.line].bold = true
-  } else if (key === '*' && cursorPosition.value.charRange === 0) {
-    textLines.value[cursorPosition.value.line].chip = true
-  } else {
-    textLines.value[cursorPosition.value.line].text = insertAt(
-      textLines.value[cursorPosition.value.line].text,
-      key,
-      cursorPosition.value.charRange
-    )
-    await new Promise((resolve) => setTimeout(resolve, 1))
-    cursorPosition.value.charRange += 1
-  }
-  editCount.value += 1
-}
-
-const handleNewLine = async () => {
-  const line = cursorPosition.value.line
-  const charRange = cursorPosition.value.charRange
-  const initialText = textLines.value[cursorPosition.value.line].text
-
-  const firstNewLineText = initialText.slice(0, charRange)
-  const secondNewLineText = initialText.substring(charRange, initialText.length)
-
-  const newLine = { index: 440, text: secondNewLineText, bold: false, chip: false }
-
-  textLines.value.splice(line + 1, 0, newLine)
-  await new Promise((resolve) => setTimeout(resolve, 10))
-  cursorPosition.value = { line: line + 1, charRange: 0 }
-  textLines.value[line].text = firstNewLineText
-
-  editCount.value += 1
-}
-
-const keydown = async (event) => {
-  const key = event.key
-  event.preventDefault()
-  console.log('Keydown : ', key)
-
-  if (key.length === 1) {
-    await handleWrite(key)
-  } else if (key === 'Enter') {
-    await handleNewLine()
-  } else if (key === 'Backspace') {
-    await handleBackspace()
-  } else if (key === 'ArrowRight') {
-    if (cursorPosition.value.charRange === textLines.value[cursorPosition.value.line].text.length) {
-      cursorPosition.value = { line: cursorPosition.value.line + 1, charRange: 1 }
-    } else {
-      cursorPosition.value.charRange += 1
-    }
-  } else if (key === 'ArrowLeft') {
-    if (cursorPosition.value.charRange === 0) {
-      cursorPosition.value = {
-        line: cursorPosition.value.line - 1,
-        charRange: textLines.value[cursorPosition.value.line - 1].text.length
-      }
-    } else {
-      cursorPosition.value.charRange -= 1
-    }
-  }
-  lastKeyPress.value = key
+const handleKeydown = (event) => {
+  keydown(event, cursorPosition)
 }
 
 const cursorCoordinates = ref({ left: '0px', right: '0px' })
 
-const cursorPosition = ref({ line: undefined, charRange: undefined })
+const cursorPosition = ref({ line: undefined, startOffset: undefined, endOffset: undefined })
 
-const textLines = ref([])
 const parseTextLines = (text) => {
   return text.split('\n').map((text, index) => {
     let bold = false
@@ -230,17 +120,11 @@ const formatText = () => {
     .map((line) => (line.bold ? '#' : '') + (line.chip ? '*' : '') + line.text)
     .join('\n')
 }
-const element = ref(null)
 
 onMounted(() => {
+
   componentUuid.value = uuidv4()
   textLines.value = parseTextLines(props.text)
-
-  element.value = document.getElementById('editor-interface')
-  element.focus
-  element.value.addEventListener('blur', () => {
-    element.value.focus()
-  })
   const routerView = document.getElementById('router-view')
   routerView.addEventListener('scroll', () => {
     getCursorCoordinates()
---
+++ src/composables/useWrite.ts
@@ -0,0 +1,165 @@
+import { ref, type Ref } from 'vue'
+
+type Cursor = {
+  line: number
+  startOffset: number
+  endOffset: number
+}
+
+type Line = {
+    index: number
+    text: string
+    bold: boolean
+    chip: boolean
+}
+
+const textLines = ref<Line[]>([])
+const lastKeyPress = ref<string | null>(null)
+
+const insertAt = (original: string, insertion: string, index: number) => {
+  return original.slice(0, index) + insertion + original.slice(index)
+}
+
+const removeAt = (original: string, index: number) => {
+  return original.slice(0, index - 1) + original.slice(index)
+}
+
+const editCount = ref<number>(0)
+
+const handleBackspace = async (cursorPosition: Ref<Cursor>) => {
+  if (cursorPosition.value.startOffset !== 0) {
+    textLines.value[cursorPosition.value.line].text = removeAt(
+      textLines.value[cursorPosition.value.line].text,
+      cursorPosition.value.startOffset
+    )
+    cursorPosition.value.startOffset -= 1
+  } else {
+    if (textLines.value[cursorPosition.value.line].chip) {
+      textLines.value[cursorPosition.value.line].chip = false
+      editCount.value += 1
+      return
+    }
+    if (textLines.value[cursorPosition.value.line].chip) {
+      textLines.value[cursorPosition.value.line].chip = false
+      editCount.value += 1
+      return
+    }
+    if (cursorPosition.value.line === 0) return
+
+    await new Promise((resolve) => setTimeout(resolve, 10))
+    cursorPosition.value = {
+      line: cursorPosition.value.line - 1,
+      startOffset: textLines.value[cursorPosition.value.line - 1].text.length,
+      endOffset: textLines.value[cursorPosition.value.line - 1].text.length
+    }
+    textLines.value[cursorPosition.value.line].text +=
+      textLines.value[cursorPosition.value.line + 1].text
+    textLines.value.splice(cursorPosition.value.line + 1, 1)
+  }
+  editCount.value += 1
+}
+
+const handleWrite = async (key: string, cursorPosition: Ref<Cursor>) => {
+  if (lastKeyPress.value === 'Dead') {
+    switch (key) {
+      case 'e':
+        key = 'ê'
+        break
+      case 'o':
+        key = 'ô'
+        break
+      case 'a':
+        key = 'â'
+        break
+      case 'u':
+        key = 'û'
+        break
+    }
+  }
+  if (key === '#' && cursorPosition.value.startOffset === 0) {
+    textLines.value[cursorPosition.value.line].bold = true
+  } else if (key === '*' && cursorPosition.value.startOffset === 0) {
+    textLines.value[cursorPosition.value.line].chip = true
+  } else {
+    textLines.value[cursorPosition.value.line].text = insertAt(
+      textLines.value[cursorPosition.value.line].text,
+      key,
+      cursorPosition.value.startOffset
+    )
+    await new Promise((resolve) => setTimeout(resolve, 1))
+    cursorPosition.value.startOffset += 1
+  }
+  editCount.value += 1
+}
+
+const handleNewLine = async (cursorPosition: Ref<Cursor>) => {
+  const line = cursorPosition.value.line
+  const startOffset = cursorPosition.value.startOffset
+  const initialText = textLines.value[cursorPosition.value.line].text
+
+  const firstNewLineText = initialText.slice(0, startOffset)
+  const secondNewLineText = initialText.substring(startOffset, initialText.length)
+
+  const newLine = { index: 440, text: secondNewLineText, bold: false, chip: false }
+
+  textLines.value.splice(line + 1, 0, newLine)
+  await new Promise((resolve) => setTimeout(resolve, 10))
+  cursorPosition.value = { line: line + 1, startOffset: 0, endOffset: 0 }
+  textLines.value[line].text = firstNewLineText
+
+  editCount.value += 1
+}
+
+const localClipboard = ref<string>('')
+const copyToClipboard = (cursorPosition: Ref<Cursor>) => {
+  const copiedText = textLines.value[cursorPosition.value.line].text.slice(
+    cursorPosition.value.startOffset,
+    cursorPosition.value.endOffset
+  )
+  console.log('CopiedText : ', copiedText)
+}
+
+const keydown = async (event: any, cursorPosition: Ref<Cursor>) => {
+  const key = event.key
+  event.preventDefault()
+  console.log('Keydown : ', key)
+
+  if (key === 'c' && lastKeyPress.value === 'Control') {
+    copyToClipboard(cursorPosition)
+    lastKeyPress.value = key
+    return
+  }
+
+  if (key.length === 1) {
+    await handleWrite(key, cursorPosition)
+  } else if (key === 'Enter') {
+    await handleNewLine(cursorPosition)
+  } else if (key === 'Backspace') {
+    await handleBackspace(cursorPosition)
+  } else if (key === 'ArrowRight') {
+    if (cursorPosition.value.startOffset === textLines.value[cursorPosition.value.line].text.length) {
+      cursorPosition.value = { line: cursorPosition.value.line + 1, startOffset: 1, endOffset: 1 }
+    } else {
+      cursorPosition.value.startOffset += 1
+    }
+  } else if (key === 'ArrowLeft') {
+    if (cursorPosition.value.startOffset === 0) {
+      cursorPosition.value = {
+        line: cursorPosition.value.line - 1,
+        startOffset: textLines.value[cursorPosition.value.line - 1].text.length,
+          endOffset:textLines.value[cursorPosition.value.line - 1].text.length 
+      }
+    } else {
+      cursorPosition.value.startOffset -= 1
+    }
+  }
+  lastKeyPress.value = key
+}
+
+export function useWrite () {
+    return {
+        textLines,
+        keydown,
+        editCount
+    }
+}
