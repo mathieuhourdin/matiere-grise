076164cd515d80182af82257b847b47d11259f84
mathieu <mathieu@fleursdici.fr>
2024-05-28 14:21:24 +0200

    can ask for remove line breaks option in fileConverter

--- src/composables/useFileConverter.ts
+++ src/composables/useFileConverter.ts
@@ -1,7 +1,15 @@
 import { fetchWrapper } from '@/helpers'
 
-const postFileConversion = async (file: any, target_format: string = "ppdc") => {
-  const response = await fetchWrapper.post(`/file_conversion?target_format=${target_format}`, file, 'multipart/form-data')
+const postFileConversion = async (
+  file: any,
+  target_format: string = 'ppdc',
+  removeLineBreaks: boolean = false
+) => {
+  const response = await fetchWrapper.post(
+    `/file_conversion?target_format=${target_format}&remove_line_breaks=${removeLineBreaks}`,
+    file,
+    'multipart/form-data'
+  )
   return response.data
 }
 
--- src/views/SpipConversionView.vue
+++ src/views/SpipConversionView.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="flex flex-col p-4">
     <div class="text-sm mb-2">
-      Conversion au format SPIP : ajouter le contenu à partir d'un fichier .docx (.doc ne marche pas) puis copier (icon en haut à droite) coller
-      dans spip.
-      Le gras est mis en gras, et les parties entre guillements sont mises en italique. Attention aux insertions de discours indirect dans les citations, qui doivent être remises en non italique.
+      Conversion au format SPIP : ajouter le contenu à partir d'un fichier .docx (.doc ne marche
+      pas) puis copier (icon en haut à droite) coller dans spip. Le gras est mis en gras, et les
+      parties entre guillements sont mises en italique. Attention aux insertions de discours
+      indirect dans les citations, qui doivent être remises en non italique.
     </div>
+    <div class="text-sm mb-2">
+      Pour les fichiers copiés collés depuis un pdf, sélectionnez l'option pour enlever les sauts de
+      ligne. La mise en gras ne fonctionne pas pour cette option (problème technique inconnu). Un
+      simple saut de ligne est supprimé, un double saut de ligne est transformé en un simple saut de
+      ligne.
+    </div>
+    <CheckboxInput class="mb-2" v-model="removeLineBreaks" label="Enlever les sauts de ligne" />
     <input
       id="file"
       type="file"
@@ -27,9 +35,11 @@
 import { ref } from 'vue'
 import { useFileConverter } from '@/composables/useFileConverter'
 import SelectionTextInterface from '@/components/SelectionTextInterface.vue'
+import CheckboxInput from '@/components/Ui/CheckboxInput.vue'
 
 const { postFileConversion } = useFileConverter()
 const fileConversion = ref(null)
+const removeLineBreaks = ref(false)
 const uploadFile = async (event) => {
   const file = event.target.files[0]
 
@@ -37,7 +47,7 @@ const uploadFile = async (event) => {
 
   formData.append(file.name, file)
 
-  const response = await postFileConversion(formData, 'spip')
+  const response = await postFileConversion(formData, 'spip', removeLineBreaks.value)
   fileConversion.value = response
 }
 </script>
