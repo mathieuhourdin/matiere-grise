81cc087ab6ac931acba1a34bdaa43211f6d52a54
mathieu <mathieu@fleursdici.fr>
2025-12-06 13:06:03 +0100

    deploy dist

--- dist/assets/useFileConverter-0c2a9617.js
+++
@@ -1,1 +0,0 @@
-import{k as o}from"./index-86fc8a3f.js";const s=async(e,r="ppdc",t=!1)=>(await o.post(`/file_conversion?target_format=${r}&remove_line_breaks=${t}`,e,"multipart/form-data")).data;function p(){return{postFileConversion:s}}export{p as u};
